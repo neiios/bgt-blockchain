0904ec19ecef216bee20bdb12737c4664cc9d2a4
Igor <68248740+richard96292@users.noreply.github.com>
2022-10-25 18:04:16 +0300

    Add a timestamp to transaction

--- transaction.hpp
+++ transaction.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <chrono>
 #include <cstdint>
 #include <functional>
 #include <hash.hpp>
@@ -13,17 +14,24 @@ class Transaction {
     std::string sender;
     // public key of the user who gets the transaction
     std::string address;
+    uint64_t timestamp;
     uint64_t amount;
 
   public:
     Transaction(const std::string& s, const std::string& ad, const uint64_t& am)
-        : sender(s), address(ad), amount(am) {
+        : sender(s),
+          address(ad),
+          timestamp(
+              std::chrono::system_clock::now().time_since_epoch().count()),
+          amount(am) {
         Hash h;
-        id = h.hashString(sender + address + std::to_string(amount));
+        id = h.hashString(sender + address + std::to_string(timestamp) +
+                          std::to_string(amount));
     }
 
     friend std::ostream& operator<<(std::ostream& os, const Transaction& t) {
-        os << t.id << '/' << t.sender << '/' << t.address << '/' << t.amount;
+        os << t.id << '/' << t.sender << '/' << t.address << '/' << t.timestamp
+           << '/' << t.amount;
         return os;
     }
 };
