7d1437074a76741cfd36df0ebbe9f83beca6ea19
Igor <68248740+richard96292@users.noreply.github.com>
2022-11-03 10:59:14 +0200

    Change defines

--- defines.hpp
+++ defines.hpp
@@ -3,21 +3,27 @@
 #define USER_COUNT 1000
 #define MIN_BALANCE 100
 #define MAX_BALANCE 1000000
-
 #define TRANSACTION_COUNT 10000
-#define MIN_TRANSACTION 10
-#define MAX_TRANSACTION 10000
-
+#define MIN_TRANSACTION 100
+#define MAX_TRANSACTION 100000
 #define TRANSACTIONS_IN_BLOCK 100
-#define DIFFICULTY_TARGET 4
+
+#define DIFFICULTY_TARGET 5
 #define VERSION 1
 
-#define MINER_COUNT 5
+#define MINER_COUNT 12
 
-// comment it out if you want to see less output
+// more output
 // #define BE_VERBOSE
 // #define VERBOSE_ADD
 // #define VERBOSE_GENERATION
 // #define VERBOSE_MERKLE
 // #define VERBOSE_REMOVE
 // #define VERBOSE_MINING
+// #define USER_COUNT 5
+// #define MIN_BALANCE 100
+// #define MAX_BALANCE 1000
+// #define TRANSACTION_COUNT 30
+// #define MIN_TRANSACTION 42
+// #define MAX_TRANSACTION 420
+// #define TRANSACTIONS_IN_BLOCK 9
