7f628822976fae5febfc646cc709173a5f5d7b32
Igor <68248740+richard96292@users.noreply.github.com>
2022-11-07 16:01:00 +0200

    User merkle tree from the libbitcoin library

--- Makefile
+++ Makefile
@@ -7,7 +7,7 @@ CC=g++
 OPT=-O3 -march=native
 # OPT=-O0
 DEPFLAGS=-MP -MD
-CFLAGS=-Wall -Wextra -std=gnu++20 -g $(foreach D,$(INCDIRS),-I$(D)) $(OPT) $(DEPFLAGS)
+CFLAGS=-Wall -Wextra -std=gnu++2a -g $(foreach D,$(INCDIRS),-I$(D)) $(OPT) $(DEPFLAGS)
 
 CFILES=$(foreach D,$(CODEDIRS),$(wildcard $(D)/*.cpp))
 OBJECTS=$(patsubst %.cpp,%.o,$(CFILES))
@@ -16,10 +16,10 @@ DEPFILES=$(patsubst %.cpp,%.d,$(CFILES))
 all: $(BINARY)
 
 $(BINARY): $(OBJECTS)
-	$(CC) -o $@ $^ -fopenmp
+	$(CC) -o $@ $^ -fopenmp -I/usr/local/include -L/usr/local/lib -lbitcoin -lboost_chrono -lboost_date_time -lboost_filesystem -lboost_locale -lboost_program_options -lboost_regex -lboost_system -lboost_thread -lpthread -lrt -ldl -lsecp256k1
 
 %.o: %.cpp
-	$(CC) $(CFLAGS) -c -o $@ $< -fopenmp
+	$(CC) $(CFLAGS) -c -o $@ $< -fopenmp -I/usr/local/include -L/usr/local/lib -lbitcoin -lboost_chrono -lboost_date_time -lboost_filesystem -lboost_locale -lboost_program_options -lboost_regex -lboost_system -lboost_thread -lpthread -lrt -ldl -lsecp256k1
 
 clean:
 	rm -rf $(BINARY) $(OBJECTS) $(DEPFILES)
--- merkleTree.hpp
+++ merkleTree.hpp
@@ -8,6 +8,10 @@
 #include "hash.hpp"
 #include "transaction.hpp"
 
+// merkle.cpp
+#include <bitcoin/bitcoin.hpp>
+// Merkle Root Hash
+
 class MerkleTree {
   private:
     Hash h;
@@ -17,6 +21,47 @@ class MerkleTree {
         return h.hashString(s.str());
     }
 
+    bc::hash_digest create_merkle(bc::hash_list& merkle) {
+        // Stop if hash list is empty or contains one element
+        if (merkle.empty())
+            return bc::null_hash;
+        else if (merkle.size() == 1)
+            return merkle[0];
+        // While there is more than 1 hash in the list, keep looping...
+        while (merkle.size() > 1) {
+            // If number of hashes is odd, duplicate last hash in the list.
+            if (merkle.size() % 2 != 0)
+                merkle.push_back(merkle.back());
+            // List size is now even.
+            assert(merkle.size() % 2 == 0);
+            // New hash list.
+            bc::hash_list new_merkle;
+            // Loop through hashes 2 at a time.
+            for (auto it = merkle.begin(); it != merkle.end(); it += 2) {
+                // Join both current hashes together (concatenate).
+                bc::data_chunk concat_data(bc::hash_size * 2);
+                auto concat = bc::serializer<decltype(concat_data.begin())>(
+                    concat_data.begin());
+                concat.write_hash(*it);
+                concat.write_hash(*(it + 1));
+                // Hash both of the hashes.
+                bc::hash_digest new_root = bc::bitcoin_hash(concat_data);
+                // Add this to the new list.
+                new_merkle.push_back(new_root);
+            }
+            // This is the new list.
+            merkle = new_merkle;
+#ifdef VERBOSE_MERKLE
+            std::cout << "Current merkle hash list:" << std::endl;
+            for (const auto& hash : merkle)
+                std::cout << " " << bc::encode_base16(hash) << std::endl;
+            std::cout << std::endl;
+#endif
+        }
+        // Finally we end up with a single item.
+        return merkle[0];
+    }
+
   public:
     /**
      * @brief Function to find a merkle root hash
@@ -24,30 +69,16 @@ class MerkleTree {
      * @param txs hashes of all transactions in a block
      */
     std::string findMerkleRootHash(const std::vector<std::string>& txs) {
-        std::vector<std::string> tempTxs;
-        // base case
-        if (txs.size() == 1) {
-#ifdef VERBOSE_MERKLE
-            std::cout << "Root hash: " << txs.at(0) << std::endl;
-#endif
-            return txs.at(0);
+        // a vector to store hashes in a proper format
+        bc::hash_list tx_hashes;
+        for (const auto& tx : txs) {
+            char msg[65];
+            strcpy(msg, tx.c_str());
+            tx_hashes.push_back(bc::hash_literal(msg));
         }
 
-#ifdef VERBOSE_MERKLE
-        std::cout << "Current level (hash count): " << txs.size() << " ";
-        for (const auto& tx : txs)
-            std::cout << tx << " ";
-        std::cout << std::endl;
-#endif
-
-        for (size_t i = 0; i < txs.size(); i += 2) {
-            if (i + 1 < txs.size()) {
-                tempTxs.push_back(hashPair(txs.at(i), txs.at(i + 1)));
-            } else {
-                tempTxs.push_back(hashPair(txs.at(i), txs.at(i)));
-            }
-        }
-
-        return findMerkleRootHash(tempTxs);
+        // create a merkle root hash
+        const bc::hash_digest merkle_root = create_merkle(tx_hashes);
+        return bc::encode_hash(merkle_root);
     }
 };
