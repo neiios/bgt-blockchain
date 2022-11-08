90a21b56340fa1fa72e545f63b1b5f45158364df
Igor <68248740+richard96292@users.noreply.github.com>
2022-11-08 17:24:08 +0200

    Convert python scripts to use main function

--- python/block_hash.py
+++ python/block_hash.py
@@ -5,24 +5,6 @@ import re
 import hashlib
 from binascii import unhexlify, hexlify
 
-# Create a connection to local Bitcoin Core node
-p = RawProxy()
-
-# enter block with a check
-max_height = p.getblockcount()
-print("The highest block right now is " + str(max_height))
-while True:
-  try:
-    block_height = input('Enter block height: ')
-    block_height = int(block_height)
-    if (block_height >= 0 and block_height <= max_height): break
-    raise ValueError
-  except ValueError:
-    print("The number is invalid.")
-
-blockhash = p.getblockhash(block_height)
-block = p.getblock(blockhash)
-
 def convert_num_to_le(num, len):
   return hexlify(num.to_bytes(len, "little")).decode("utf-8")
 
@@ -36,27 +18,49 @@ def hash_block_header(str):
   hash = hashlib.sha256(hashlib.sha256(header_bin).digest()).digest()
   return hexlify(hash[::-1]).decode("utf-8")
 
-# header fields
-version = convert_num_to_le(block['version'], 4)
-if (block_height != 0): hashPrevBlock = convert_str_to_le(block['previousblockhash'])
-else: hashPrevBlock = convert_str_to_le("0000000000000000000000000000000000000000000000000000000000000000")
-hashMerkleRoot = convert_str_to_le(block['merkleroot'])
-time = convert_num_to_le(block['time'], 4)
-bits = convert_str_to_le(block['bits'])
-nonce = convert_num_to_le(block['nonce'], 4)
+def main():
+  # Create a connection to local Bitcoin Core node
+  p = RawProxy()
 
-# hash header
-header = hash_block_header(version + hashPrevBlock + hashMerkleRoot + time + bits + nonce)
-if (blockhash == header): print(f"The block hash is correct.")
-else: print(f"The block hash is incorrect.")
-print(f"Blockhash 1: {blockhash:>70}\nBlockhash 2: {header:>70}")
+  # enter block with a check
+  max_height = p.getblockcount()
+  print("The highest block right now is " + str(max_height))
+  while True:
+    try:
+      block_height = input('Enter block height: ')
+      block_height = int(block_height)
+      if (block_height >= 0 and block_height <= max_height): break
+      raise ValueError
+    except ValueError:
+      print("The number is invalid.")
 
-# DEBUG
-# print(blockhash)
-# pprint(block)
-# print(version)
-# print(hashPrevBlock)
-# print(hashMerkleRoot)
-# print(time)
-# print(bits)
-# print(nonce)
+  blockhash = p.getblockhash(block_height)
+  block = p.getblock(blockhash)
+
+  # header fields
+  version = convert_num_to_le(block['version'], 4)
+  if (block_height != 0): hashPrevBlock = convert_str_to_le(block['previousblockhash'])
+  else: hashPrevBlock = convert_str_to_le("0000000000000000000000000000000000000000000000000000000000000000")
+  hashMerkleRoot = convert_str_to_le(block['merkleroot'])
+  time = convert_num_to_le(block['time'], 4)
+  bits = convert_str_to_le(block['bits'])
+  nonce = convert_num_to_le(block['nonce'], 4)
+
+  # hash header
+  header = hash_block_header(version + hashPrevBlock + hashMerkleRoot + time + bits + nonce)
+  if (blockhash == header): print(f"The block hash is correct.")
+  else: print(f"The block hash is incorrect.")
+  print(f"Blockhash 1: {blockhash:>70}\nBlockhash 2: {header:>70}")
+
+  # DEBUG
+  # print(blockhash)
+  # pprint(block)
+  # print(version)
+  # print(hashPrevBlock)
+  # print(hashMerkleRoot)
+  # print(time)
+  # print(bits)
+  # print(nonce)
+
+if __name__ == "__main__":
+  main()
